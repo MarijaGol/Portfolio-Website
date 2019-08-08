3d9f5ad9bffa052fa42535f67621cb31e9463baf
MarijaGol <52384646+MarijaGol@users.noreply.github.com>
2019-08-08 14:30:23 -0400

    Adding toggle to the menu

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+console.log ("Welcome to my portfolio website")
+
+function menuToggle() {
+   var x = document.getElementById('myNavtoggle');{
+    x.className += 'responsive';
+} else {
+    x.className = 'navtoggle';
+  }
+}
