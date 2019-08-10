efdbeb6421acdb889308684359a3fab7971c52a0
MarijaGol <52384646+MarijaGol@users.noreply.github.com>
2019-08-10 14:33:08 -0400

    update 2.3
    Updated navigation with a toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 console.log ("Welcome to my portfolio website")
 
 function menuToggle() {
-   var x = document.getElementById('myNavtoggle');{
-    x.className += 'responsive';
-} else {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
     x.className = 'navtoggle';
   }
 }
